666100bfbd471ce5b2693b9649ed707475847757
Albhieri Cristoff Villa Contreras <alcrivico@gmail.com>
2024-11-12 09:23:14 -0600

    Se agregó la ventana VerMonederosView

---
+++ SAMS.UI/Views/VerMonederosView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAMS.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for VerMonederosView.xaml
+    /// </summary>
+    public partial class VerMonederosView : Window
+    {
+        public VerMonederosView()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBarControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (e.OriginalSource is FrameworkElement element &&
+                (element.Name == "MinusLogo" ||
+                element.Name == "MaximizeLogo" ||
+                element.Name == "ExitLogo"))
+            {
+                return;
+            }
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+
+        }
+
+        private void TitleBarControl_WindowStateChangeRequested(object sender, WindowState e)
+        {
+            this.WindowState = e;
+        }
+
+    }
+    
+}
