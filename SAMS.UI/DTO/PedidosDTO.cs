57f59f18a3f0b33533512e26e226aea71c91040b
Victoria Moyano <Soyunpokemonytuno@hotmail.com>
2024-11-23 21:58:35 -0600

    Pantalla principal de VerPedidos

---
+++ SAMS.UI/DTO/PedidosDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMS.UI.DTO
+{
+    public class PedidosDTO
+    {
+        public string noPedido {  get; set; }
+        public string nombreProveedor { get; set; }
+        public DateTime fechaEntrega { get; set; }
+        public string nombreEstado { get; set; }
+
+    }
+}
